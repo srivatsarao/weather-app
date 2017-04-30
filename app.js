28fc80001a4e9ab5d28e1bcf901dbff7e5252720
srivatsarao <srivats.rao@gmail.com>
2017-04-29 22:09:41 -0500

    added icons & more result attributes

--- app.js
+++ app.js
@@ -1,59 +1,67 @@
 // declare packages
 //const yargs = require('yargs');
 const axios = require('axios');
-const express = require ('express');
-const hbs = require ('hbs');
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require("body-parser");
+const path = require('path');
+const port = process.env.port || 3000;
 
 //start express & set directory paths
 var app = express();
 //hbs.registerPartials(__dirname+'/views/partials');
-app.set('view engine','hbs');
-app.use(express.static(__dirname+'/views'));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/', (req,res)=> {
-  res.render('home.hbs',{
-    message: "hello"
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//render page
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: "Weather Application"
   });
 });
-/*const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
+
+//get location request data
+app.post('/', (req, res) => {
+  let location = req.body.location;
+  let encodedAddress = encodeURIComponent(location);
+  let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+  //call Dark Sky API 
+  axios.get(geocodeUrl).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+      throw new Error('Unable to find that address.');
     }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;*/
 
-/*var encodedAddress = encodeURIComponent(argv.address);
-var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+    let lat = response.data.results[0].geometry.location.lat;
+    let lng = response.data.results[0].geometry.location.lng;
+    const apiKey = '5e37b9c3dae7a201ae898c00f026c6b2';
+    let weatherUrl = `https://api.forecast.io/forecast/${apiKey}/${lat},${lng}`;
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+  }).then((response) => {
+    let temperature = response.data.currently.temperature;
+    let apparentTemperature = response.data.currently.apparentTemperature;
+    let icon = response.data.currently.icon;
+    let humidity = response.data.currently.humidity;
+    let windSpeed = response.data.currently.windSpeed;
+    console.log(`temperature: ${temperature}. apparent temp: ${apparentTemperature}. icon: ${icon}. humidity: ${humidity}. windspeed: ${windSpeed}.`);
+    res.json({ temperature, apparentTemperature, icon, humidity, windSpeed });
+  }).catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+      console.log('Unable to connect to API servers.');
+    } else {
+      console.log(e.message);
+      res.json('null');
+    }
+  });
+});
 
-axios.get(geocodeUrl).then((response) => {
-  if (response.data.status === 'ZERO_RESULTS') {
-    throw new Error('Unable to find that address.');
-  }
-
-  var lat = response.data.results[0].geometry.location.lat;
-  var lng = response.data.results[0].geometry.location.lng;
-  const apiKey = '5e37b9c3dae7a201ae898c00f026c6b2';
-  var weatherUrl = `https://api.forecast.io/forecast/${apiKey}/${lat},${lng}`;
-  console.log(response.data.results[0].formatted_address);
-  return axios.get(weatherUrl);
-}).then((response) => {
-  var temperature = response.data.currently.temperature;
-  var apparentTemperature = response.data.currently.apparentTemperature;
-  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
-}).catch((e) => {
-  if (e.code === 'ENOTFOUND') {
-    console.log('Unable to connect to API servers.');
-  } else {
-    console.log(e.message);
-  }
-});*/
-
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+//listen on port
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
